0fa2bf651afce8276784651dba7d11d264b7942b
Misbah <Misbahsaleh0@gmail.com>
2025-08-24 22:02:29 +0100

    made the second timer live

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement) {
-  let losAngelesDateElement = document.querySelector("#los-angeles .date");
-  let losAngelesTimeElement = document.querySelector("#los-angeles .time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = document.querySelector("#los-angeles .date");
+    let losAngelesTimeElement = document.querySelector("#los-angeles .time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("Do MMMM YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("Do MMMM YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = document.querySelector("#sydney .date");
+    let sydneyTimeElement = document.querySelector("#sydney .time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+
+    sydneyDateElement.innerHTML = sydneyTime.format("Do MMMM YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
-
-let sydneyElement = document.querySelector("#sydney");
-if (sydneyElement) {
-  let sydneyDateElement = document.querySelector("#sydney .date");
-  let sydneyTimeElement = document.querySelector("#sydney .time");
-  let sydneyTime = moment().tz("Australia/Sydney");
-
-  sydneyDateElement.innerHTML = sydneyTime.format("Do MMMM YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
-}
+updateTime();
+setInterval(updateTime, 1000);
